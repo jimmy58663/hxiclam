02a8d319404c1f2269789991f8921c27e62b4f7d
Joshua Chase <jimmy58663@hotmail.com>
2026-08-08 16:52:13 -0700

    Initial font scale fix and begintabitem

--- Ashita_v4/hxiclam.lua
+++ Ashita_v4/hxiclam.lua
@@ -138,6 +138,31 @@ local function format_int(number)
     end
 end
 
+local function push_scaled_font(scale)
+    if (type(scale) ~= 'number' or scale == 1.0) then return false; end
+    if (type(imgui.PushFont) ~= 'function' or type(imgui.PopFont) ~= 'function' or
+        type(imgui.GetFont) ~= 'function' or type(imgui.GetFontSize) ~=
+        'function') then return false; end
+
+    local font = imgui.GetFont();
+    if (font == nil) then return false; end
+
+    local font_size = imgui.GetFontSize();
+    if (type(font_size) ~= 'number' or font_size <= 0) then return false; end
+
+    local dpi_scale = 1.0;
+    if (type(imgui.GetWindowDpiScale) == 'function') then
+        local window_dpi_scale = imgui.GetWindowDpiScale();
+        if (type(window_dpi_scale) == 'number' and window_dpi_scale > 0) then
+            dpi_scale = window_dpi_scale;
+        end
+    end
+
+    local font_size_unscaled = font_size / dpi_scale;
+    imgui.PushFont(font, font_size_unscaled * scale);
+    return true;
+end
+
 function WriteLog(logtype, item)
     -- Current log types supported are drop and turnin
     local logdir = nil
@@ -558,15 +583,15 @@ local function render_editor()
 
         if (imgui.BeginTabBar('##hxiclam_tabbar',
                               ImGuiTabBarFlags_NoCloseWithMiddleMouseButton)) then
-            if (imgui.BeginTabItem('General', nil)) then
+            if (imgui.BeginTabItem('General', nil, 0)) then
                 render_general_config(settings);
                 imgui.EndTabItem();
             end
-            if (imgui.BeginTabItem('Item Price', nil)) then
+            if (imgui.BeginTabItem('Item Price', nil, 0)) then
                 render_item_price_config(settings);
                 imgui.EndTabItem();
             end
-            if (imgui.BeginTabItem('Item Weight', nil)) then
+            if (imgui.BeginTabItem('Item Weight', nil, 0)) then
                 render_item_weight_config(settings);
                 imgui.EndTabItem();
             end
@@ -873,11 +898,24 @@ ashita.events.register('d3d_present', 'present_cb', function()
         local total_worth = 0;
         local bucket_total = 0;
 
-        imgui.SetWindowFontScale(hxiclam.settings.font_scale[1] + 0.1);
+        local display_font_pushed = false;
+        if (hxiclam.settings.font_scale[1] ~= 1.0) then
+            display_font_pushed = push_scaled_font(
+                                      hxiclam.settings.font_scale[1]);
+        end
+        local header_font_pushed = push_scaled_font(
+                                       hxiclam.settings.font_scale[1] + 0.1)
         imgui.Text('Bucket Stats:');
-        imgui.SetWindowFontScale(hxiclam.settings.bucket_weight_font_scale[1]);
+        if (header_font_pushed) then imgui.PopFont(); end
+
+        local bucket_weight_font_pushed = false;
+        if (hxiclam.settings.bucket_weight_font_scale[1] ~= 1.0) then
+            bucket_weight_font_pushed = push_scaled_font(hxiclam.settings
+                                                             .bucket_weight_font_scale[1]);
+        end
         imgui.Text('Bucket Weight: ');
         imgui.SameLine();
+
         if ((hxiclam.settings.bucket_capacity - hxiclam.settings.bucket_weight) <=
             hxiclam.settings.bucket_weight_crit_threshold[1]) then
             imgui.TextColored(hxiclam.settings.bucket_weight_crit_color,
@@ -893,7 +931,8 @@ ashita.events.register('d3d_present', 'present_cb', function()
             imgui.Text(tostring(hxiclam.settings.bucket_weight) .. '/' ..
                            hxiclam.settings.bucket_capacity);
         end
-        imgui.SetWindowFontScale(hxiclam.settings.font_scale[1]);
+
+        if (bucket_weight_font_pushed) then imgui.PopFont(); end
 
         imgui.Text('Dig Timer: ');
         imgui.SameLine();
@@ -973,9 +1012,10 @@ ashita.events.register('d3d_present', 'present_cb', function()
 
         if (hxiclam.settings.session_view > 0) then
             imgui.Separator();
-            imgui.SetWindowFontScale(hxiclam.settings.font_scale[1] + 0.1);
+            header_font_pushed = push_scaled_font(
+                                     hxiclam.settings.font_scale[1] + 0.1)
             imgui.Text('Session Stats:');
-            imgui.SetWindowFontScale(hxiclam.settings.font_scale[1]);
+            if (header_font_pushed) then imgui.PopFont(); end
             imgui.Text('Buckets Cost: ' ..
                            format_int(hxiclam.settings.bucket_count *
                                           hxiclam.settings.clamming.bucket_cost[1]));
@@ -1021,6 +1061,8 @@ ashita.events.register('d3d_present', 'present_cb', function()
                         format_int(hxiclam.gil_per_hour) .. ' gph)');
             end
         end
+
+        if (display_font_pushed) then imgui.PopFont(); end
     end
     imgui.End();
 
